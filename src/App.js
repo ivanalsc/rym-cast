fd2b6351fa525240b6b6715b088275214f46515d
ivanalsc <ivanasoscordero@gmail.com>
2020-10-01 09:58:43 -0300

    Status added to characters

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import Card from './components/Card';
 function App() {
   const [cast, setCast] = useState([]);
 
-  const url = 'https://rickandmortyapi.com/api/character/1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20';
+  const url = 'https://rickandmortyapi.com/api/character/?limit=100';
 
   useEffect(()=>{
     fetch(url)
     .then(res => res.json())
     .then(body => {
-      setCast(body)
+      setCast(body.results)
     })
   },[])
 
@@ -22,7 +22,7 @@ function App() {
      {
        cast.map(character => (
         
-         <Card key ={character.id} name={character.name} type ={character.species} img={character.image} />
+         <Card key ={character.id} name={character.name} type ={character.species} img={character.image} status={character.status} />
        ))
      }
      </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
-const Card= ({name, type, img}) => {
+const Card= ({name, type, img, status}) => {
     return (
         <div className='card'>
             <h2>{name}</h2>
             <img alt={name} src={img}/>
             <p>Specie: {type} </p>
+            <p className={status === 'Alive' ? 'alive' : status === 'Dead' ? 'dead' : 'unknown'}>{status}</p>
 
             
         </div>
